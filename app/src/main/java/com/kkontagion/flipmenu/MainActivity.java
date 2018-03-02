2ceb6a6593c7a4cf0ab52875ccf337e1971086e7
NarDd <1601037@SIT.Singaporetech.edu.sg>
2018-03-02 23:53:06 +0800

    Added camera features and save images to fancaidan folder on external storage

--- app/src/main/java/com/kkontagion/flipmenu/MainActivity.java
+++ app/src/main/java/com/kkontagion/flipmenu/MainActivity.java
@@ -1,8 +1,13 @@
 package com.kkontagion.flipmenu;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,10 +17,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+    private static final int CAMERA_PIC_REQUEST = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +35,23 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        Button clickButton = (Button) findViewById(R.id.bt_translate);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA},MY_CAMERA_REQUEST_CODE);
+                }
+                else if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},MY_CAMERA_REQUEST_CODE);
+                }
+                else{
+                    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                    startActivityForResult(intent, CAMERA_PIC_REQUEST);
+                }
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +61,62 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Camera Permission Granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == 200)
+        {
+            if(resultCode == RESULT_OK){
+                Long tsLong = System.currentTimeMillis()/1000;
+                String ts = tsLong.toString();
+                String filename = ts+".png";
+
+                File directory = new File(Environment.getExternalStorageDirectory() + "/fancaidan");
+
+                if (!directory.exists()) {
+                    if (directory.mkdirs()) {
+                        Log.d("hi", "Successfully created the parent dir:" + directory.getName());
+                    } else {
+                        Log.d("bye", "Failed to create the parent dir:" + directory.getName());
+                    }
+                }
+
+                File dest = new File(directory, filename);
+
+                Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+                try {
+                    FileOutputStream out = new FileOutputStream(dest);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+                    out.flush();
+                    out.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "Picture Not taken", Toast.LENGTH_LONG);
+            }
+
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Picture Not taken", Toast.LENGTH_LONG);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
