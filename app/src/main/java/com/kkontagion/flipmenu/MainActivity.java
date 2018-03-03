b4b50d1a44baacd65ba0f699cd41ba501a730635
NarDd <1601037@SIT.Singaporetech.edu.sg>
2018-03-03 17:44:52 +0800

    Removed commented codes

--- app/src/main/java/com/kkontagion/flipmenu/MainActivity.java
+++ app/src/main/java/com/kkontagion/flipmenu/MainActivity.java
@@ -32,9 +32,6 @@ import static android.Manifest.permission_group.CAMERA;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static final int MY_CAMERA_REQUEST_CODE = 100;
-    private static final int READ_REQUEST_CODE = 200;
-    private static final int WRITE_REQUEST_CODE = 201;
     private static final int RequestPermissionCode = 7;
 
     private String imagePath;
@@ -56,7 +53,6 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View v) {
                 if(CheckingPermissionIsEnabledOrNot())
                 {
-//                    Toast.makeText(MainActivity.this, "All Permissions Granted Successfully", Toast.LENGTH_LONG).show();
                     accessCamera();
                 }
                 else {
@@ -106,55 +102,6 @@ public class MainActivity extends AppCompatActivity
             Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
             intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, outputFileUri);
             startActivityForResult(intent, RequestPermissionCode);
-
-//        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-//            requestPermissions(new String[]{Manifest.permission.CAMERA},MY_CAMERA_REQUEST_CODE);
-//            camera_request = true;
-//        }
-//
-//        else if (checkSelfPermission(WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-//            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE},WRITE_REQUEST_CODE);
-//            write_request = true;
-//        }
-//
-//        else if (checkSelfPermission(READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-//            requestPermissions(new String[]{READ_EXTERNAL_STORAGE},READ_REQUEST_CODE);
-//            read_request = true;
-//        }
-//
-//        else{
-//            System.out.print("in");
-//            File mainDirectory = new File( Environment.getExternalStorageDirectory() + "/fancaidan");
-//            if (!mainDirectory.exists()) {
-//                if (mainDirectory.mkdirs()) {
-//                    Log.d("Main Directory Created", "Successfully created the parent dir:" + mainDirectory.getName());
-//                } else {
-//                    Log.d("Directory Not Created", "Failed to create the parent dir:" + mainDirectory.getName());
-//                }
-//            }
-//
-//            File photosFileDirectory = new File(Environment.getExternalStorageDirectory() + "/fancaidan/photos");
-//            if (!photosFileDirectory.exists()) {
-//                if (photosFileDirectory.mkdirs()) {
-//                    Log.d("Photos Dir Created", "Successfully created the parent dir:" + photosFileDirectory.getName());
-//                } else {
-//                    Log.d("Photos Dir Not Created", "Failed to create the parent dir:" + photosFileDirectory.getName());
-//                }
-//            }
-//
-//            Long tsLong = System.currentTimeMillis()/1000;
-//            String ts = tsLong.toString();
-//            filename = ts+".png";
-//
-//            imagePath = photosFileDirectory.getAbsolutePath() + "/" + filename;
-//            File imageFile = new File(imagePath);
-//
-//            Uri outputFileUri = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".provider",imageFile);
-//
-//            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-//            intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, outputFileUri);
-//            startActivityForResult(intent, MY_CAMERA_REQUEST_CODE);
-//        }
     }
 
     private void RequestMultiplePermission() {
@@ -186,27 +133,6 @@ public class MainActivity extends AppCompatActivity
             }
 
         }
-//        if (requestCode == RequestPermissionCode) {
-//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                Toast.makeText(this, "Camera Permission Granted", Toast.LENGTH_LONG).show();
-//            } else {
-//                Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_LONG).show();
-//            }
-//        }
-//        if (requestCode == WRITE_REQUEST_CODE) {
-//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                Toast.makeText(this, "WRITE Permission Granted", Toast.LENGTH_LONG).show();
-//            } else {
-//                Toast.makeText(this, "WRITE Permission Denied", Toast.LENGTH_LONG).show();
-//            }
-//        }
-//        if (requestCode == READ_REQUEST_CODE) {
-//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                Toast.makeText(this, "WRITE Permission Granted", Toast.LENGTH_LONG).show();
-//            } else {
-//                Toast.makeText(this, "WRITE Permission Denied", Toast.LENGTH_LONG).show();
-//            }
-//        }
     }
 
     public boolean CheckingPermissionIsEnabledOrNot() {
