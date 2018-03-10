098c65596d10f8ac35ce4c5e0b3c9091bea54b17
Konstantin K <eekkonstantin@gmail.com>
2018-03-10 15:35:29 +0800

    Speech function updated.

--- app/src/main/java/com/kkontagion/flipmenu/OrderActivity.java
+++ app/src/main/java/com/kkontagion/flipmenu/OrderActivity.java
@@ -2,6 +2,7 @@ package com.kkontagion.flipmenu;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
@@ -12,39 +13,68 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kkontagion.flipmenu.adapters.ItemAdapter;
 import com.kkontagion.flipmenu.objects.Item;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class OrderActivity extends AppCompatActivity {
 
     ArrayList<Item> orders;
+    ItemAdapter adapter;
+    TextToSpeech t1;
 
     RecyclerView rv;
     TextView tvClear, tvEmpty;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // TODO: SPEAK
-            }
-        });
+        fab = findViewById(R.id.fab);
 
         orders = getIntent().getParcelableArrayListExtra("items");
         rv = findViewById(R.id.rv);
-        final ItemAdapter adapter = new ItemAdapter(true, getBaseContext(), orders);
+        adapter = new ItemAdapter(true, getBaseContext(), orders);
         rv.setAdapter(adapter);
 
         tvClear = findViewById(R.id.bt_clear);
         tvEmpty = findViewById(R.id.tv_empty);
+
+
+
+        if (orders.size() == 0) {
+            tvEmpty.setVisibility(View.VISIBLE);
+            tvClear.setVisibility(View.GONE);
+        }
+
+        setupInteractions();
+    }
+
+    private void setupInteractions() {
+        //bernard tts
+        t1=new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.ENGLISH);
+                }
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), prepareSpeech(),Toast.LENGTH_SHORT).show();
+                t1.speak(prepareSpeech(),TextToSpeech.QUEUE_FLUSH,null,null);
+            }
+        });
+        //bernard tts END
 
         tvClear.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,18 +84,17 @@ public class OrderActivity extends AppCompatActivity {
                 tvEmpty.setVisibility(View.VISIBLE);
             }
         });
-
-        if (orders.size() == 0) {
-            tvEmpty.setVisibility(View.VISIBLE);
-            tvClear.setVisibility(View.GONE);
-        }
     }
 
     /**
      * Prepare String for TTS to say.
-     * @return
      */
     private String prepareSpeech() {
+        // TODO Bernard: try, if too fast, use this in the for loop instead
+        /**
+         * mytts.speak(snippet, QUEUE_ADD, null);
+         * mytts.playSilentUtterance(2000, QUEUE_ADD, null);
+         */
         StringBuilder sb = new StringBuilder();
 
         for (Item o : orders) {
@@ -96,8 +125,19 @@ public class OrderActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 
+    //bernard tts
+    @Override
+    protected void onDestroy() {
+        if(t1 != null) {
+            t1.stop();
+            t1.shutdown();
+            Log.d(getClass().getSimpleName(), "TTS Destroyed");
+        }
+        super.onDestroy();
+    }
+
     /**
-     * Prepare
+     * Prepare to send order statuses back to Menu page.
      * @return
      */
     private Intent prepareBack() {
