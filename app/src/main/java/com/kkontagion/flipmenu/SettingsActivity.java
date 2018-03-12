c02c616b4aeda0b4c5e82c08e5282da88c6a0fbf
Konstantin K <eekkonstantin@gmail.com>
2018-03-12 13:39:30 +0800

    Color scheme updated. Some bugs fixed.

--- app/src/main/java/com/kkontagion/flipmenu/SettingsActivity.java
+++ app/src/main/java/com/kkontagion/flipmenu/SettingsActivity.java
@@ -181,14 +181,14 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             bindPreferenceSummaryToValue(findPreference("pref_language"));
         }
 
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            int id = item.getItemId();
-            if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
+//        @Override
+//        public boolean onOptionsItemSelected(MenuItem item) {
+//            int id = item.getItemId();
+//            if (id == android.R.id.home) {
+//                startActivity(new Intent(getActivity(), SettingsActivity.class));
+//                return true;
+//            }
+//            return super.onOptionsItemSelected(item);
+//        }
     }
 }
